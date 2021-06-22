09a43b5c7e252593ecfcc0b229fd5e0e0e9fce2a
Artsiom Panko <artsiom.panko@gmail.com>
2021-06-22 18:14:39 +0300

    next update

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const button = document.querySelector('.img__text'),
+        currentImage = document.querySelector('.dog_img');
+
+    const imgesSrc = ['img/dog/dog1.jpg',
+        'img/dog/dog2.jpg',
+        'img/dog/dog3.jpg',
+        'img/dog/dog4.jpg',
+        'img/dog/dog5.jpg'
+    ];
+
+    currentImage.src = imgesSrc[Math.floor(Math.random() * imgesSrc.length)];
+
+    button.addEventListener('click', () => {
+        setNextImage();
+    })
+
+    function setNextImage() {
+        var nextImage = imgesSrc[Math.floor(Math.random() * imgesSrc.length)];
+
+        while (nextImage === currentImage.getAttribute("src")) {
+            nextImage = imgesSrc[Math.floor(Math.random() * imgesSrc.length)];
+            console.log('nextImage:', nextImage);
+        }
+
+        currentImage.src = nextImage;
+    }
+})
